49912072e82042e396e780fca68aa195878b8cee
alicechinez <83326408+alicechinez@users.noreply.github.com>
2022-06-11 11:44:02 +0800

    feat: import redis adaptor

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { createServer } = require('http')
 const { Server } = require('socket.io')
+const { createAdapter } = require('@socket.io/redis-adapter')
+const { createClient } = require('redis')
 const userHandler = require('./loaders/userHandler')
 
 const PORT = 3000
@@ -14,12 +16,15 @@ const io = new Server(httpServer, {
   }
 })
 
+const pubClient = createClient({ url: 'redis://localhost:6379' })
+const subClient = pubClient.duplicate()
+
 const onConnection = socket => {
   userHandler(io, socket)
 }
 
-io.on('connection', onConnection)
-
-httpServer.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+  io.adapter(createAdapter(pubClient, subClient))
+  io.listen(PORT)
+  io.on('connection', onConnection)
 })
